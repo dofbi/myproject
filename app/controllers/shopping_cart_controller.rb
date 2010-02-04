9408c6661826664aa3b320a1980a1e63836e8def
Mamadou Diagne <genova@dofbi.com>
2010-02-04 03:42:22 +0000

    une redirection

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,5 +2,10 @@ class ShoppingCartController < ApplicationController
   def show
     puts 'I an the controller.'
   end
+  
+  def display_cart
+    puts 'I the display_cart action'
+    redirect_to :action => "show"
+  end
 
 end
